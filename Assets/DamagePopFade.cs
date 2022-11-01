2eff45d042ab7d6de130b7d0ab4c14b7cbbbd3ec
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2022-10-31 23:11:50 -0700

    Added faster pop, left or right of camera

--- Assets/DamagePopFade.cs
+++ Assets/DamagePopFade.cs
@@ -1,47 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class DamagePopFade : MonoBehaviour
 {
-    private float _damage;
     private TextMeshPro _text;
+
     private Vector3 _direction;
     private Vector3 _startPosition;
+
+    private bool _startFade;
+
+    private float _damage;
+    private float _speed;
 
 
     public void Setup(float damageDone)
     {
         _damage = damageDone;
         _text.text = _damage.ToString();
+
+        _speed = Random.Range(24, 34);
     }
 
     void Awake()
     {
         _text = GetComponent<TextMeshPro>();
-        _direction = this.transform.position;
-        Vector3 test = (Camera.main.transform.position - _startPosition).normalized;
-        _direction = new Vector3(test.x, 0, test.z);
-        _direction = Quaternion.Euler(0, 90, 0) * _direction;
+        _startPosition = this.transform.position;
+
+        // Find direction from origin to camera
+        _direction = (Camera.main.transform.position - _startPosition).normalized;
+
+        // Flatten direction y
+        _direction = new Vector3(_direction.x, 0, _direction.z);
+
+        // Find perpendicular to direction, going either left or right relative from camera
+        int[] range = new int[] { -1, 1 };
+        _direction = Quaternion.Euler(-45, range[Random.Range(0, 2)] * 90, 0) * _direction;
+
+        StartCoroutine(startTheFade());
     }
+
+    // Start fade in half a second
+    private IEnumerator startTheFade()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _startFade = true;
+    } 
 
     private void Update()
     {
-        Debug.DrawRay(_startPosition, _direction, Color.cyan, 1, false);
 
-        transform.position += _direction * Time.deltaTime;
-        //_fadeSpeed *= 2 * Time.deltaTime;
+        // Move left or right relative from camera
+        transform.position += _direction * _speed * Time.deltaTime;
+
+        _speed *= 0.625f;
 
         Color currentColor = _text.color;
 
-        if (currentColor.a > 0)
+        // Fade alpha then destroy half a second after creation
+        if (_startFade)
         {
-            _text.color = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a - 0.6f * Time.deltaTime);
-        }
-        else
-        {
-            Destroy(gameObject);
+            if (currentColor.a > 0)
+            {
+                _text.color = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a - 5f * Time.deltaTime);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
--- Assets/Scripts/DamageManager.cs
+++ Assets/Scripts/DamageManager.cs
@@ -9,19 +9,22 @@ public class DamageManager : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     public void ProcessDamage(float damage)
     {
+        // Find vector towards camera from origin
         Quaternion CameraAngle = Quaternion.LookRotation(this.transform.position - Camera.main.transform.position);
         CameraAngle = new Quaternion(0, CameraAngle.y, 0, CameraAngle.w);
+
+        // Create Damage pop facing camera
         GameObject DamagePop =  Instantiate(damagePop,this.transform.position, CameraAngle);
         DamagePop.GetComponent<DamagePopFade>().Setup(damage);
 
         health -= damage;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             Destroy(gameObject, 4f);
         }
