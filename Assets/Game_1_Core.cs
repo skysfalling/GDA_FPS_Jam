642cecd533d1bf7c4de6aa09ba56154842818ae3
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2022-12-12 21:47:10 -0900

    Added Laser Grid and correct spawning positions

--- Assets/Game_1_Core.cs
+++ Assets/Game_1_Core.cs
@@ -9,21 +9,31 @@ public class Game_1_Core : MonoBehaviour
     public int TargetsLeft = 20;
     public int TargetsHit = 0;
     Target_Air_Controller currentTarget;
+    LineRenderer lineRenderer;
+    float lineWidth = 0f;
 
     public Target_Air_Controller Target;
 
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
     void Update()
     {
-        if (gameStarted)
+        if (gameStarted && lineWidth < 0.25f)
         {
-
+            lineRenderer.startWidth = lineWidth;
+            lineRenderer.endWidth = lineWidth;
+            lineWidth += 0.05f;
         }
     }
 
     private void spawnTarget()
     {
-        currentTarget = Instantiate(Target, new Vector3(Random.Range(0,10), 2, Random.Range(0, 10)), Quaternion.identity);
+        Vector3 spawnPos = new Vector3(0f,2f,0f) + transform.position + Quaternion.Euler(0f, Random.Range(-40f, 45f), 0f) * Vector3.right * 15;
+        Quaternion spawnRot = Quaternion.LookRotation((transform.position - spawnPos).normalized) * Quaternion.Euler(0,90,0);
+        currentTarget = Instantiate(Target, spawnPos, spawnRot);
         currentTarget.RunOnHit.AddListener(targetHit);
     }
 
