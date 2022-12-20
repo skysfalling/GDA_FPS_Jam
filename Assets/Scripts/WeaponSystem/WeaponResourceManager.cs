532b1387153cf64053b31797b3028071c4bfa4cb
Calex Raffield <clxrffdman@gmail.com>
2022-12-20 15:41:11 -0800

    did some work on the weapon resource manager

--- Assets/Scripts/WeaponSystem/WeaponResourceManager.cs
+++ Assets/Scripts/WeaponSystem/WeaponResourceManager.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class WeaponInfo
+{
+    public string id;
+    public string weaponPrefabPath;
+    public string meshPath;
+
+
+    public WeaponInfo(string _id, string _weaponPrefabPath, string _meshPath)
+    {
+        this.id = _id;
+        this.weaponPrefabPath = _weaponPrefabPath;
+        this.meshPath = _meshPath;
+
+    }
+}
 
 public class WeaponResourceManager : MonoBehaviour
 {
+
+    public List<WeaponInfo> allFoundWeaponsInfo = new List<WeaponInfo>();
+
+    private void Awake()
+    {
+        FindAllResourceWeapons();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +43,19 @@ public class WeaponResourceManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FindAllResourceWeapons()
+    {
+        var allWeaponInfo = Resources.LoadAll("", typeof(TextAsset)).Cast<TextAsset>().ToArray();
+        foreach (var t in allWeaponInfo)
+        {
+            if(t.name == "weapon_info")
+            {
+                allFoundWeapons.Add(JsonUtility.FromJson<WeaponInfo>(t.text));
+            }
+            
+        }
+            
     }
 }
