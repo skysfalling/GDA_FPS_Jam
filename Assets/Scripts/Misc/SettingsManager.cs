e1a5ed1b5c99e0cfe4187d4dd71e8392231e7a88
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2023-01-12 13:47:35 -0800

    Working Volume and FOV var slider, doesn't adjust FOV

--- Assets/Scripts/Misc/SettingsManager.cs
+++ Assets/Scripts/Misc/SettingsManager.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
+    // Settings
+    public float Volume;
+    public float FOV;
+
+    // Trackers
     bool settingsOpen = false;
     float oldTimeScale;
 
+    // Dependenices 
     public PlayerController player;
     public GameObject settingsUICanvas;
+    public AudioMixer audioMixer;
+    public GameObject volumeSlider;
+    public GameObject fovSlider;
+
+    TextMeshProUGUI volumeText;
+    TextMeshProUGUI fovText;
 
     private void Start()
     {
         settingsUICanvas.SetActive(false);
+        volumeText = volumeSlider.GetComponent<TextMeshProUGUI>();
+        fovText = fovSlider.GetComponent<TextMeshProUGUI>();
+
+        // Set default Volume
+        audioMixer.SetFloat("MasterVolume", (Mathf.Log10(Volume)*20));
+        volumeText.text = (Volume*100).ToString("0")+"%";
+        volumeSlider.GetComponent<Slider>().value = Volume;
+
+        // Set default FOV
+        fovText.text = FOV.ToString("0");
+        fovSlider.GetComponent<Slider>().value = FOV;
     }
 
     private void Update()
@@ -43,5 +69,18 @@ public class SettingsManager : MonoBehaviour
                 settingsUICanvas.SetActive(false);
             }
         }
+    }
+
+    public void SetVolume(float Volume)
+    {
+        audioMixer.SetFloat("MasterVolume", (Mathf.Log10(Volume) * 20));
+        this.Volume = Volume;
+        volumeText.text = (Volume * 100).ToString("0") + "%";
+    }
+
+    public void SetFov(float FOV)
+    {
+        this.FOV = FOV;
+        fovText.text = FOV.ToString("0");
     }
 }
