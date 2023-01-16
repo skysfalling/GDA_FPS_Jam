83207e72be3946ca608a87e96e9a1d669905b090
Enrico <60024711+ewidodo@users.noreply.github.com>
2023-01-16 04:47:07 -0800

    Little bit more work

--- Assets/Resources/SunflowerGun/Scripts/ChargeShotBehavior.cs
+++ Assets/Resources/SunflowerGun/Scripts/ChargeShotBehavior.cs
@@ -5,8 +5,19 @@ using UnityEngine.InputSystem;
 
 public class ChargeShotBehavior : MonoBehaviour
 {
-    public void Fire(InputAction.CallbackContext context)
+    public FormObject formObject;
+    public float chargeTime = 0.533f;
+
+
+    private bool fireButtonPressed;
+    private float chargeDuration;
+
+
+    /// <summary>
+    /// Calculates the percentage of charge at a given point in time
+    /// </summary>
+    public float GetChargePercentage()
     {
-        Debug.Log("test");
+        return formObject._currentPrimaryCooldown / chargeTime;
     }
 }
